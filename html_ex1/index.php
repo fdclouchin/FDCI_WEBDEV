15164f8eec7523832ff00a779ebab3447313d7e2
fdclouchin <67951113+fdclouchin@users.noreply.github.com>
2022-03-22 21:33:55 +0800

    master: initial commit

---
+++ html_ex1/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Nickey's Hobbys</title>
+</head>
+<style>
+	html{
+		font-family: Courier;
+	}
+</style>
+<body>
+	<!-- <div>This is div</div>
+
+	<span>This is a span</span><span> This is a span 2</span>
+
+	<p>This is a paragraph</p>
+	<p>This is a paragraph 2</p>
+	<ol>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+	</ol>
+
+	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+	</ul>
+
+	<table>
+		<tr>
+			<td>row column1</td>
+			<td>row column2</td>
+		</tr>
+		<tr>
+			<td>row2 column1</td>
+			<td>row2 column2</td>
+		</tr>
+	</table> -->
+	<!-- table>tr*2>td*2 -->
+	<a href="resume.php">Resume</a>
+	<a href="chessboard.php">Chessboard</a>
+	<table border = "1" width="800">
+		<!-- this is a banner -->
+		<tr>
+			<td colspan="3">
+				<table>
+					<td><img width = "800" src="images/20220226_061132.jpg" alt="Nickey's image"></td>
+					<td><img width = "800" src="images/20220226_061132.jpg" alt="Nickey's image"></td>
+				</table>
+			</td>
+		</tr>
+
+		<tr>
+			<td>1</td>
+			<td rowspan="2">2</td>
+			<td>3</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>3</td>
+		</tr>
+
+		<!-- this is for the items -->
+		<tr>
+			<td bgcolor="blue">
+				<div>
+					<h4>Top Places</h4>
+					<ol>
+						<li>Cebu 1</li>
+						<li>Cebu 2</li>
+						<li>Cebu 3</li>
+						<li>Cebu 4</li>
+						<li>Cebu 5</li>
+					</ol>
+				</div>
+			</td>
+			<td>
+				<div>
+					<h4>Top Websites</h4>
+					<ol>
+						<li>
+							<a href="www.google.com">Google</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://nativecamp.net">Nativecamp</a>
+						</li>
+						<li>Foods 3</li>
+						<li>Foods 4</li>
+						<li>Foods 5</li>
+					</ol>
+				</div>
+			</td>
+			<td>
+				<div>
+					<h4>Top Girls</h4>
+					<ol>
+						<li>Girls 1</li>
+						<li>Girls 2</li>
+						<li>Girls 3</li>
+						<li>Girls 4</li>
+						<li>Girls 5</li>
+					</ol>
+				</div>
+			</td>
+		</tr>
+
+		<!-- this is for the blog 1 -->
+		<tr>
+			<td colspan = "3">
+				<ul>
+					<li>
+						<h4>Touring</h4>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum excepturi quas autem, velit deserunt nihil voluptas eos deleniti, fugiat magni libero doloremque exercitationem odio, assumenda corrupti quasi nam ullam quidem.</p>
+					</li>
+					<li>
+						<h4>Travels</h4>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum excepturi quas autem, velit deserunt nihil voluptas eos deleniti, fugiat magni libero doloremque exercitationem odio, assumenda corrupti quasi nam ullam quidem.</p>
+					</li>
+				</ul>
+			</td>
+		</tr>
+	
+		<!-- this is for the footer -->
+		<tr>
+			<td colspan="3">Copyright 2022</td>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ html_ex1/resume.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+</head>
+<body>
+    <table border = "1" width="800">
+        <tr>
+            <td colspan="2">Name: Louchin Var C. Cejar</td>
+        </tr>
+
+        <tr>
+            <td colspan="2">Address: Cebu City</td>
+        </tr>
+
+        <tr>
+            <td>
+                Age: 25
+            </td>
+            <td>
+                Gender: Male
+            </td>
+           
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <h3>Educational Background:</h3>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="1">i. Primary Education</td>
+            <td colspan="1">FCC</td>
+        </tr>
+
+        <tr>
+            <td colspan="1">ii. Secondary Education</td>
+            <td colspan="1">FCU</td>
+        </tr>
+
+        <tr>
+            <td colspan="1">iii. Tertiary Education</td>
+            <td colspan="1">FCU</td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <h4>Work Experience:</h4>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="1">FDCi</td>
+            <td colspan="1">July 2020-Present</td>
+        </tr>
+        <tr>
+            <td colspan="1">ACN</td>
+            <td colspan="1">May 2019-May2020</td>
+        </tr>
+        <tr>
+            <td colspan="1">ROGOMI Inc.</td>
+            <td colspan="1">Aug 2018-Dec 2018</td>
+        </tr>
+        <tr>
+            <td colspan="1">Gurango Software Corp.</td>
+            <td colspan="1">May 2017-Dec 2017</td>
+        </tr>
+    </table>
+</body>
+</html>
